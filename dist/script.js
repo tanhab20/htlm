098667700c7da8334a46ab7714c3b22e57d29368
tanhab20 <tanhab20@htl-kaindorf.at>
2025-01-14 11:11:48 +0100

    push

---
+++ dist/script.js
@@ -0,0 +1,7 @@
+// TypeScript Datei (script.ts)
+const button = document.getElementById('btn');
+if (button) {
+    button.addEventListener('click', () => {
+        alert('Hallo von TypeScript!');
+    });
+}
